7390867a9dec2a79614fd407492a62eacde53239
Ali Kurnaz <alikurnaz_26@windowslive.com>
2025-10-23 21:51:37 +0300

    feat: Initialize Firebase in main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core_initialize/firebase_options.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    demoProjectId: "demo-${DefaultFirebaseOptions.currentPlatform.projectId}",
+  );
+
   runApp(const MainApp());
 }
 
@@ -10,11 +18,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      home: Scaffold(body: Center(child: Text('Hello World!'))),
     );
   }
 }
